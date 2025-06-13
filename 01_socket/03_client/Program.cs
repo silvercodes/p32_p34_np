6fae342fc29315024204d159eb5bc333075401b8
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-06-13 21:31:07 +0300

    add: tcp socket

---
+++ 01_socket/03_client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Channels;
+
+const int serverPort = 8080;
+// const string serverIp = "172.20.10.5";
+const string serverIp = "127.0.0.1";
+
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+try
+{
+    Console.Write("> ");
+    string? message = Console.ReadLine();
+
+    if (message is null)
+        throw new ArgumentException("Message is empty");
+
+    socket.Connect(serverEndPoint);                     // BLOCKING
+    socket.Send(Encoding.UTF8.GetBytes(message));       // BLOCKING
+
+    byte[] buffer = new byte[1024];
+    int byteCount = 0;
+    StringBuilder messageBuilder = new StringBuilder();
+    do
+    {
+        byteCount = socket.Receive(buffer);             // BLOCKING
+        messageBuilder.Append(Encoding.UTF8.GetString(buffer, 0, byteCount));
+    } while (socket.Available > 0);
+
+    Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: Response: {messageBuilder.ToString()}");
+
+    socket.Shutdown(SocketShutdown.Both);
+    // socket.Close();
+
+    Console.WriteLine("Connection closed");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+}
--- 01_socket/04_server_async/Program.cs
+++ 01_socket/04_server_async/Program.cs
@@ -9,22 +9,28 @@ const int port = 8080;
 const string serverIp = "127.0.0.1";
 
 using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
 IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(serverIp), port);
 
 try
 {
     socket.Bind(endPoint);
-    socket.Listen();
+    socket.Listen(10);
 
     Console.WriteLine($"Server started at {serverIp}:{port}");
 
-    while(true)
+    await RunProcessingAsync();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+}
+
+
+async Task RunProcessingAsync()
+{
+    while (true)
     {
-        Socket remoteSocket = socket.Accept();            // BLOCKING
-        // 1. Абстракция над установленным соединением с конкретным клиентом
-        // 2. Уникальный идентификатор: Локальный IP/PORT + клиентский IP/PORT
-        //                      192.168.1.10:8080 + 192.168.1.20:54123
+        Socket remoteSocket = await socket.AcceptAsync();
         Console.WriteLine("Connection established...");
 
         byte[] buffer = new byte[1024];
@@ -32,24 +38,16 @@ try
         StringBuilder messageBuilder = new StringBuilder();
         do
         {
-            byteCount = remoteSocket.Receive(buffer);
+            byteCount = await remoteSocket.ReceiveAsync(buffer);
             messageBuilder.Append(Encoding.UTF8.GetString(buffer, 0, byteCount));
         } while (remoteSocket.Available > 0);
 
         Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: {messageBuilder.ToString()}");
 
-        Thread.Sleep(2000);
+        await Task.Delay(2000);
 
-        // string response = "Hello from server! All OK!";
-
-        string response = @"HTTP/1.1 200 OK
-Content-Type: text/html; charset=utf-8
-Connection: close
-
-<h1 style='color:red;'>Vasia</h1>";
-
-
-        remoteSocket.Send(Encoding.UTF8.GetBytes(response));
+        string response = "Hello from server! All OK!";
+        await remoteSocket.SendAsync(Encoding.UTF8.GetBytes(response));
 
         remoteSocket.Shutdown(SocketShutdown.Both);
         remoteSocket.Close();
@@ -57,9 +55,4 @@ Connection: close
         Console.WriteLine("Connection closed");
     }
 }
-catch (Exception ex)
-{
-    Console.WriteLine($"ERROR: {ex.Message}");
-}
-
 
