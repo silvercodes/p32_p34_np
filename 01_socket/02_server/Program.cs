ec92d5f1d7a52c1f99a66d2d855908691e771246
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-06-13 16:26:53 +0300

    add: socket

---
+++ 01_socket/02_server/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using System.Net.Sockets;
+
+const int port = 8080;
+const string serverIp = "172.20.10.5";
+
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
